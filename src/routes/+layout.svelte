6ba8b70d0efc01cfc521fac517fb5ba98b17e579
Aaron Happe <aaronmichaelhappe@gmail.com>
2023-03-21 13:02:38 -0700

    feat: add Tailwind and Brandon font to project configuration

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,37 @@
+<script lang="ts">
+	import '../app.css';
+	// import 'iconify-icon';
+	// import { onMount } from 'svelte';
+
+	// onMount(() => {});
+</script>
+
+<div class="app min-h-screen w-full">
+	<!-- Header -->
+
+	<div class="site-slot p-4 sm:p-8">
+		<slot />
+	</div>
+
+	<footer class="py-2 text-center ">
+		<p>visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to learn SvelteKit</p>
+	</footer>
+</div>
+
+<style lang="postcss">
+	.app {
+		display: flex;
+		flex-direction: column;
+		background-color: var(--themegray-900);
+	}
+
+	.site-slot {
+		flex: 1;
+		background-color: var(--themegray-900);
+		@apply w-full;
+	}
+
+	footer {
+		background-color: var(--themegray-900);
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,12 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+	import { Alert } from 'flowbite-svelte';
+</script>
+
+<div class="p-8">
+	<h1>Aaron Happe</h1>
+	<p>YO</p>
+	<Alert>
+		<span class="font-medium bg-primaryPink">Info alert!</span> Change a few things up and try submitting
+		again.
+	</Alert>
+</div>
