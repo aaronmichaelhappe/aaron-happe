f3b1e3271ac6ce8ffd82659724d774724a92c674
Aaron Happe <aaronmichaelhappe@gmail.com>
2023-03-23 19:41:37 -0700

    init intro work section

---
+++ src/routes/Work.svelte
@@ -0,0 +1,15 @@
+<script>
+	import Img from '../images/abst1.png';
+</script>
+
+<div class="flex flex-wrap justify-between p-4">
+	<div class="w-full px-2 py-2 xs:w-1/2 lg:w-1/3">
+		<img src={Img} alt="Description 1" class="w-full" />
+	</div>
+	<div class="hidden px-2 py-2 xs:block xs:w-1/2 lg:w-1/3">
+		<img src={Img} alt="Description 2" class="w-full" />
+	</div>
+	<div class="w-full bg-gray-800 px-2 py-2 xs:w-1/2 lg:w-1/3">
+		<img src={Img} alt="Description 3" class="w-full" />
+	</div>
+</div>
