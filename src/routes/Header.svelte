018beed2181b6ef2382343228efd9b94e2769e04
Aaron Happe <aaronmichaelhappe@gmail.com>
2023-03-25 07:51:25 -0700

    feature: hamburger menu functionality

--- src/routes/Header.svelte
+++ src/routes/Header.svelte
@@ -1,32 +1,33 @@
-<!-- <div class="p-4">
-	<div class="relative">
-		<input type="checkbox" class="peer hidden" id="hamburger-checkbox" />
-		<label
-			for="hamburger-checkbox"
-			class="hamburger-lines block h-8 w-10 cursor-pointer peer-checked:rotate-180 peer-checked:transform"
-		>
-			<span
-				class="line line1 absolute top-0 block h-[.2rem] w-10 bg-white transition-transform duration-300 ease-in-out"
-			/>
-			<span
-				class="line line2 absolute block h-[.2rem] w-10 bg-white transition-opacity duration-300 ease-in-out"
-			/>
-			<span
-				class="line line3 absolute bottom-0 block h-[.2rem] w-10 bg-white transition-transform duration-300 ease-in-out"
-			/>
-		</label>
-	</div>
-</div> -->
 <script lang="ts">
+	import { onMount } from 'svelte';
 	import AnimatedUnderlinedLink from '../lib/AnimatedUnderlinedLink/AnimatedUnderlinedLink.svelte';
 	import { createEventDispatcher } from 'svelte';
 
-	// elements for each section
 	export let currentSection = '';
 
 	let workEl: any;
 	let aboutEl: any;
 	let blogEl: any;
+	let checkbox: HTMLInputElement;
+	let menuOpen = false;
+
+	let mobileMenuClasses = '';
+
+	function updateMenuClasses() {
+		const isMdOrAbove = window.matchMedia('(min-width: 768px)').matches;
+		mobileMenuClasses = isMdOrAbove
+			? ''
+			: ` fixed shadow border-l border-gray-200 left-[100%] right-[150%] w-1/2 top-0 h-[100vh] bg-themegray-100 transition-transform duration-300 ease-in-out ${
+					menuOpen ? 'transform-none' : '-translate-x-[100%]'
+			  } md:flex`;
+	}
+
+	onMount(() => {
+		menuOpen = checkbox.checked;
+		updateMenuClasses();
+	});
+
+	window.addEventListener('resize', updateMenuClasses);
 
 	$: stuckItem = currentSection;
 
@@ -36,11 +37,51 @@
 		stuckItem = event.detail.to;
 		dispatch('scrollto', event.detail.to);
 	}
+
+	function toggleMenu() {
+		if (checkbox) {
+			checkbox.checked = !checkbox.checked;
+			menuOpen = checkbox.checked;
+			updateMenuClasses();
+		}
+	}
 </script>
 
-<nav>
-	<ul class="flex justify-end text-lg">
-		<div>
+<!-- hamburger menu icon -->
+<div
+	class="pointer-events-none relative z-20 flex justify-end p-4 md:hidden"
+	on:click={toggleMenu}
+	on:keydown={(e) => (e.key === 'Enter' || e.key === ' ') && toggleMenu()}
+>
+	<div class="pointer-events-auto relative ">
+		<input
+			type="checkbox"
+			bind:this={checkbox}
+			checked
+			class="peer hidden"
+			id="hamburger-checkbox"
+		/>
+		<label
+			for="hamburger-checkbox"
+			class="hamburger-lines block h-8 w-10 cursor-pointer peer-checked:rotate-180 peer-checked:transform"
+		>
+			<span
+				class="line line1 absolute top-0 block h-0.5 w-10 bg-themegray-900 transition-transform duration-300 ease-in-out"
+			/>
+			<span
+				class="line line2 absolute block h-0.5 w-10 bg-themegray-900 opacity-0 transition-opacity duration-300 ease-in-out"
+				style="top: calc(50% - 0.1rem);"
+			/>
+			<span
+				class="line line3 absolute bottom-0 block h-0.5 w-10 bg-themegray-900 transition-transform duration-300 ease-in-out"
+			/>
+		</label>
+	</div>
+</div>
+
+<nav class={`${mobileMenuClasses}`}>
+	<ul class="flex flex-col pl-4 pt-4 text-lg md:flex-row md:justify-end md:pt-0 md:pl-0">
+		<div class="cursor-pointer pb-4">
 			<AnimatedUnderlinedLink
 				bind:this={workEl}
 				name="Work"
@@ -52,7 +93,7 @@
 				on:animatedlinkclicked={(e) => handleScrollTo(e, 0)}
 			/>
 		</div>
-		<div class="ml-8">
+		<div class="cursor-pointer pb-4 md:ml-8">
 			<AnimatedUnderlinedLink
 				bind:this={aboutEl}
 				name="About"
@@ -64,7 +105,7 @@
 				on:scrollto={(e) => handleScrollTo(e, 1)}
 			/>
 		</div>
-		<div class="ml-8">
+		<div class="cursor-pointer pb-4 md:ml-8">
 			<AnimatedUnderlinedLink
 				bind:this={blogEl}
 				name="Blog"
@@ -80,18 +121,19 @@
 </nav>
 
 <style>
-	.line2 {
-		top: calc(50% - 0.1rem);
-	}
-	.peer:checked ~ .hamburger-lines .line1 {
+	.line1 {
 		transform: translateY(0.85rem) rotate(45deg);
 	}
-
-	.peer:checked ~ .hamburger-lines .line2 {
-		opacity: 0;
-	}
-
-	.peer:checked ~ .hamburger-lines .line3 {
+	.line3 {
 		transform: translateY(-0.9rem) rotate(-45deg);
+	}
+	.peer:checked ~ .hamburger-lines .line1 {
+		transform: none;
+	}
+	.peer:checked ~ .hamburger-lines .line2 {
+		opacity: 1;
+	}
+	.peer:checked ~ .hamburger-lines .line3 {
+		transform: none;
 	}
 </style>
