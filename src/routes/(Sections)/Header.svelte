dfce0a11fe19885e318fd8bc4d80603dd1d41990
Aaron Happe <aaronmichaelhappe@gmail.com>
2023-06-20 20:15:00 -0700

    remove input cb to reduce complixity and other menu adjustments

--- src/routes/(Sections)/Header.svelte
+++ src/routes/(Sections)/Header.svelte
@@ -62,10 +62,7 @@
 	}
 
 	function toggleMenu() {
-		if (checkbox) {
-			checkbox.checked = !checkbox.checked;
-			menuOpen = checkbox.checked;
-		}
+		menuOpen = !menuOpen;
 	}
 
 	onMount(() => {
@@ -80,34 +77,27 @@
 
 <!-- hamburger menu icon -->
 <div
-	class="pointer-events-none relative z-20 flex justify-end p-2 md:hidden"
+	class={`relative z-20 flex justify-end p-2 md:hidden ${menuOpen ? '' : 'opened'}`}
 	on:click={toggleMenu}
 	on:keydown={(e) => (e.key === 'Enter' || e.key === ' ') && toggleMenu()}
 >
 	<div class="pointer-events-auto fixed">
-		<input
-			type="checkbox"
-			bind:checked={menuOpen}
-			class={`peer hidden ${menuOpen ? '' : 'opened'}`}
-			id="hamburger-checkbox"
-		/>
-		<label
-			for="hamburger-checkbox"
+		<div
 			class={`hamburger-lines block h-[1.25rem] w-8 cursor-pointer ${
 				menuOpen ? 'rotate-180 transform' : ''
 			}`}
 		>
 			<span
-				class="line line1 absolute top-0 block h-[.15rem] w-8 bg-themeGray-900 transition-transform duration-300 ease-in-out"
+				class="line line1 absolute top-0 block h-[.12rem] w-8 bg-themeGray-900 transition-transform duration-300 ease-in-out"
 			/>
 			<span
-				class="line line2 absolute block h-[.15rem] w-8 bg-themeGray-900 opacity-0 transition-opacity duration-300 ease-in-out"
+				class="line line2 absolute block h-[.12rem] w-8 bg-themeGray-900 opacity-0 transition-opacity duration-300 ease-in-out"
 				style="top: calc(50% - 0.1rem);"
 			/>
 			<span
-				class="line line3 absolute bottom-0 block h-[.15rem] w-8 bg-themeGray-900 transition-transform duration-300 ease-in-out"
+				class="line line3 absolute bottom-0 block h-[.12rem] w-8 bg-themeGray-900 transition-transform duration-300 ease-in-out"
 			/>
-		</label>
+		</div>
 	</div>
 </div>
 
@@ -120,8 +110,10 @@
 		</div>
 	{/if}
 
-	<ul class="flex flex-col pl-4 pt-4 text-lg md:flex-row md:justify-end md:pt-0 md:pl-0">
-		<div class="cursor-pointer pb-4">
+	<ul
+		class="flex flex-col  pl-4 pt-4 text-lg md:flex-row md:items-center md:justify-end md:pt-0 md:pl-0"
+	>
+		<div class="cursor-pointer pb-4 md:pb-0">
 			<AnimatedUnderlinedLink
 				bind:this={workEl}
 				name="Work"
@@ -133,7 +125,7 @@
 				on:unstickallsiblings={() => (stuckItem = 'work')}
 			/>
 		</div>
-		<div class="cursor-pointer pb-4 md:ml-4 lg:ml-8">
+		<div class="cursor-pointer pb-4 md:ml-4 md:pb-0 lg:ml-8">
 			<AnimatedUnderlinedLink
 				bind:this={aboutEl}
 				name="About"
@@ -169,12 +161,16 @@
 				on:unstickallsiblings={() => (stuckItem = 'blog')}
 			/>
 		</div> -->
-		<a class="pb-4 md:ml-4 md:pb-0 lg:ml-8" href="https://github.com/aaronmichaelhappe/"
-			>{@html gitIconSvg}</a
-		>
-		<a class="md:ml-4" href="https://www.linkedin.com/in/aaron-happe-4741176a/"
-			>{@html linkedIconSvg}</a
-		>
+		<div class="flex cursor-pointer pt-4 pb-4 md:pb-0 lg:ml-8">
+			<a class="inline-block pb-4 md:pb-0 " href="https://github.com/aaronmichaelhappe/"
+				>{@html gitIconSvg}</a
+			>
+		</div>
+		<div class="flex cursor-pointer items-center pb-4 md:pb-0 lg:ml-8">
+			<a class="inline-block" href="https://www.linkedin.com/in/aaron-happe-4741176a/"
+				>{@html linkedIconSvg}</a
+			>
+		</div>
 	</ul>
 </nav>
 
@@ -185,13 +181,13 @@
 	.line3 {
 		transform: translateY(-0.6rem) rotate(-45deg);
 	}
-	.opened ~ .hamburger-lines .line1 {
+	.opened .hamburger-lines .line1 {
 		transform: none;
 	}
-	.opened ~ .hamburger-lines .line2 {
+	.opened .hamburger-lines .line2 {
 		opacity: 1;
 	}
-	.opened ~ .hamburger-lines .line3 {
+	.opened .hamburger-lines .line3 {
 		transform: none;
 	}
 </style>
